9985543efde9bc008080a8f2b3b85e8641c5333a
Vishwas Bhushan <blckcodr@gmail.com>
2022-06-05 19:51:44 +0530

    Website donjne

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,6 +12,13 @@ import next_src from 'assets/img/next.png';
 import prev_src from 'assets/img/prev.png';
 
 import team_src from 'assets/img/team.png';
+import vish from 'assets/img/vish.png';
+import shanu from 'assets/img/shanu.png';
+import leaf from 'assets/img/leaf.jpg';
+import duo from 'assets/img/duo.png';
+import derrick from 'assets/img/derrick.jpg';
+import maheen from 'assets/img/maheen.png';
+import poli from 'assets/img/poli.png';
 import discord_src from 'assets/img/discord.png';
 import twitter_src from 'assets/img/twitter.png';
 import { LazyLoadImage } from 'react-lazy-load-image-component';
@@ -56,17 +63,18 @@ const faq_lists = [
     },
     {
         title: 'How is Itsuki created and who is the artist?',
-        description: 'How is Itsuki created and who is the artist?'
+        description: 'Itsuki NFTs are designed by Derrick. Derrick is the lead artist of Itsuki and her drawings of mythical creatures. She is a professional illustrator, a 3D animator, a storyboard artist from Ukraine who has worked as Head of Design for a larger company, she is fascinated by the world of Elves, and can sometimes be seen bringing joy to the world of cosplay. Her cheerful ways and funny videos make her quickly win over hearts, and her sheer endless artistic creativity brightens up the whole team’s day when there is another WIP to see.'
     },
     {
         title: 'What is the supply & mint price?',
-        description: 'What is the supply & mint price?'
+        description: 'There will total of 6666 Itsuki be minted on the Ethereum blockchain to represents forests spirits. The mint price will be announced when we get closer to the mint.'
     },
     {
         title: 'When is the mint date?',
-        description: 'About mint data?'
+        description: '26th July 2022'
     }
 ];
+
 
 const ItuskiHome = () => {
     return (
@@ -150,7 +158,7 @@ const ItuskiHome = () => {
                                 <div className='w-75'>
                                     <div className='slider-section'>
                                         <div className='d-flex flex-column slider-description-box'>
-                                            <span className='slider-section-title'>ITUSKI <span>VISION 1</span></span>
+                                            <span className='slider-section-title'>ITUSKI <span>VISION</span></span>
                                             <span className='slider-section-description mt-5'>
                                                 We at Itsuki adore nature. We believe that giving back to nature is an important element for human growth and existence. This extends into the animal kingdom aswell.
                                                 <br /><br />
@@ -161,22 +169,73 @@ const ItuskiHome = () => {
                                 </div>
                             </div>
                         </Carousel.Item>
+                        <Carousel.Item interval={1000}>
+                            <div className='d-flex justify-content-center'>
+                                <div className='w-75'>
+                                    <div className='slider-section'>
+                                        <div className='d-flex flex-column slider-description-box'>
+                                            <span className='slider-section-title'><span>EMPOWER WOMEN ENTREPRENEURS</span></span>
+                                            <span className='slider-section-description mt-5'>
+                                                 Itsuki belives in women empowerment in Web3, hence we’re inviting all the women creators, gamers, developers and entreprenuers. Itsuki forest will provide help all the women who are willing to learn and start their own project.  
+                                                <br /><br />
+                                                Our aim is to empower more women in nft, women in tech and women in games.
+                                            </span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </Carousel.Item>
+                       
                         <Carousel.Item>
                             <div className='d-flex justify-content-center'>
                                 <div className='w-75'>
                                     <div className='slider-section'>
                                         <div className='d-flex flex-column slider-description-box'>
-                                            <span className='slider-section-title'>ITUSKI <span>VISION 2</span></span>
-                                            <span className='slider-section-description mt-5'>
-                                                We at Itsuki adore nature. We believe that giving back to nature is an important element for human growth and existence. This extends into the animal kingdom aswell.
+                                            <span className='slider-section-title'><span>Values With Nature</span></span>
+                                            <span className='slider-section-description value-nature mt-5'>
+                                                Itsuki is a Japanese word that means Tree. The Itsuki are here to represent the spirits of the forest. Our goal is to create an ecosystem where real trees will add value to the Itsuki community. Here's how:
                                                 <br /><br />
-                                                We thrive on creating the greatest organic NFT community ever to explore and implement all the possibilities of bringing technology and nature together.
+                                                Planting a real tree on every mint:  Itsuki supports carbon neutrality. We will be planting a real tree IRL on every mint and on secondary sales. Every tree will be connected to the holder's wallet and holds the Itsuki token ID.
+                                                <br /><br />
+                                                Values: Itsuki calculates the average amount of oxygen produced by the planted trees based on the rarity to airdrop $ITSUKI tokens which can be used in the ecosystem.
                                             </span>
                                         </div>
                                     </div>
                                 </div>
                             </div>
                         </Carousel.Item>
+                        <Carousel.Item interval={1000}>
+                            <div className='d-flex justify-content-center'>
+                                <div className='w-75'>
+                                    <div className='slider-section'>
+                                        <div className='d-flex flex-column slider-description-box'>
+                                            <span className='slider-section-title'>ITUSKI <span>FORESTVERSE</span></span>
+                                            <span className='slider-section-description mt-5'>
+                                                Itsuki represents the forest spirits and travels withing the roots of the forest’s trees. Itsuki holders will get all the access of the Itsuki Forestverse and will able to travel to other forests.
+                                                <br /><br />
+                                                P2E Tournaments: Delivering a great gaming experience in P2E with the physical utilities is Itsuki mission. Itsuki holders will also get the early access of all the Codemire games & projects.
+                                            </span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </Carousel.Item>
+                        <Carousel.Item interval={1000}>
+                            <div className='d-flex justify-content-center'>
+                                <div className='w-75'>
+                                    <div className='slider-section'>
+                                        <div className='d-flex flex-column slider-description-box'>
+                                            <span className='slider-section-title'>ITUSKI <span>MERCHANDISE </span></span>
+                                            <span className='slider-section-description mt-5'>
+                                                Members of the Itsuki forest will get access to exclusive Itsuki merchandise.  Itsuki will be partnering with multiple brands to bridge the physical and  the digital world with the Itsuki art style.
+                                                
+                                            </span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </Carousel.Item>
+                        
                     </Carousel>
                 </div>
 
@@ -186,24 +245,90 @@ const ItuskiHome = () => {
 
                     <div className='home-container home-sub-title-margin-top '>
                         <Row style={{ rowGap: '30px' }}>
-                            {
-                                [0, 0, 0, 0].map((_, index) => {
-                                    return (
-                                        <Col sm='6' md='4' lg='3' key={index}>
+                        <Col sm='6' md='4' lg='3'>
                                             <div className='team-member-card'>
                                                 <div style={{ background: '#D9D9D9' }}>
                                                     {/* <img src={team_src} style={{width:'100%', objectFit: 'cover'}}/> */}
-                                                    <LazyLoadImage alt='team' src={team_src} width={'100%'} effect="blur" delayTime={300}/>
+                                                    <LazyLoadImage alt='team' src={vish} width={'100%'} effect="blur" delayTime={300}/>
                                                 </div>
                                                 <div style={{ background: '#0E0B26', border: '1px solid gray', textAlign:'center'}}>
-                                                    <p className='description'>LOREM IPSUM</p>
-                                                    <p className='title'>founder</p>
+                                                    <p className='description'>Vishnft.eth</p>
+                                                    <p className='title'>Founder</p>
                                                 </div>
                                             </div>
-                                        </Col>
-                                    );
-                                })
-                            }
+                            </Col>
+                            <Col sm='6' md='4' lg='3'>
+                                            <div className='team-member-card'>
+                                                <div style={{ background: '#D9D9D9' }}>
+                                                    {/* <img src={team_src} style={{width:'100%', objectFit: 'cover'}}/> */}
+                                                    <LazyLoadImage alt='team' src={leaf} width={'100%'} effect="blur" delayTime={300}/>
+                                                </div>
+                                                <div style={{ background: '#0E0B26', border: '1px solid gray', textAlign:'center'}}>
+                                                    <p className='description'>Leafswan.eth</p>
+                                                    <p className='title'>Co-Founder</p>
+                                                </div>
+                                            </div>
+                            </Col>
+                            <Col sm='6' md='4' lg='3'>
+                                            <div className='team-member-card'>
+                                                <div style={{ background: '#D9D9D9' }}>
+                                                    {/* <img src={team_src} style={{width:'100%', objectFit: 'cover'}}/> */}
+                                                    <LazyLoadImage alt='team' src={derrick} width={'100%'} effect="blur" delayTime={300}/>
+                                                </div>
+                                                <div style={{ background: '#0E0B26', border: '1px solid gray', textAlign:'center'}}>
+                                                    <p className='description'>Derrick</p>
+                                                    <p className='title'>Lead Artist</p>
+                                                </div>
+                                            </div>
+                            </Col>
+                            <Col sm='6' md='4' lg='3'>
+                                            <div className='team-member-card'>
+                                                <div style={{ background: '#D9D9D9' }}>
+                                                    {/* <img src={team_src} style={{width:'100%', objectFit: 'cover'}}/> */}
+                                                    <LazyLoadImage alt='team' src={shanu} width={'100%'} effect="blur" delayTime={300}/>
+                                                </div>
+                                                <div style={{ background: '#0E0B26', border: '1px solid gray', textAlign:'center'}}>
+                                                    <p className='description'>Shanu.eth</p>
+                                                    <p className='title'>Developer</p>
+                                                </div>
+                                            </div>
+                            </Col>
+                            <Col sm='6' md='4' lg='3'>
+                                            <div className='team-member-card'>
+                                                <div style={{ background: '#D9D9D9' }}>
+                                                    {/* <img src={team_src} style={{width:'100%', objectFit: 'cover'}}/> */}
+                                                    <LazyLoadImage alt='team' src={poli} width={'100%'} effect="blur" delayTime={300}/>
+                                                </div>
+                                                <div style={{ background: '#0E0B26', border: '1px solid gray', textAlign:'center'}}>
+                                                    <p className='description'>Yan</p>
+                                                    <p className='title'>Artist</p>
+                                                </div>
+                                            </div>
+                            </Col>
+                            <Col sm='6' md='4' lg='3'>
+                                            <div className='team-member-card'>
+                                                <div style={{ background: '#D9D9D9' }}>
+                                                    {/* <img src={team_src} style={{width:'100%', objectFit: 'cover'}}/> */}
+                                                    <LazyLoadImage alt='team' src={duo} width={'100%'} effect="blur" delayTime={300}/>
+                                                </div>
+                                                <div style={{ background: '#0E0B26', border: '1px solid gray', textAlign:'center'}}>
+                                                    <p className='description'>DuoCrypto.eth</p>
+                                                    <p className='title'>Advisor</p>
+                                                </div>
+                                            </div>
+                            </Col>
+                            <Col sm='6' md='4' lg='3'>
+                                            <div className='team-member-card'>
+                                                <div style={{ background: '#D9D9D9' }}>
+                                                    {/* <img src={team_src} style={{width:'100%', objectFit: 'cover'}}/> */}
+                                                    <LazyLoadImage alt='team' src={maheen} width={'100%'} effect="blur" delayTime={300}/>
+                                                </div>
+                                                <div style={{ background: '#0E0B26', border: '1px solid gray', textAlign:'center'}}>
+                                                    <p className='description'>Maheen</p>
+                                                    <p className='title'>Community Manager</p>
+                                                </div>
+                                            </div>
+                            </Col>
                         </Row>
                     </div>
                 </div>
